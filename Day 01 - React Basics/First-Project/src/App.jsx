7c45f09a932f87a167e85f06c6b7f6fcbf4addaa
Vinay Parit <vinaypparit@gmail.com>
2025-11-05 13:22:59 +0530

    Basic first project done

---
+++ Day 01 - React Basics/First-Project/src/App.jsx
@@ -0,0 +1,18 @@
+import "./style.css";
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <div className="left">
+          <h1>Vinay Parit</h1>
+        </div>
+        <div className="right">
+          <h2>i'm Frontend Developer</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
