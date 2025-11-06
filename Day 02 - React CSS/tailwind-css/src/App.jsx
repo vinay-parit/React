26d7fcfdb46507a7340cb0771580add1bff6000b
Vinay Parit <vinaypparit@gmail.com>
2025-11-06 18:13:36 +0530

    Tailwind CSS Done

---
+++ Day 02 - React CSS/tailwind-css/src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-sky-800">Hello JS</h1>
+      <button className="px-4 py-2 rounded-xl bg-zinc-800 text-white">Hello There</button>
+    </div>
+  );
+}
+
+export default App
