13fe54ae612b9a6a9fb2dedc5323e005e5ccf877
Vinay Parit <vinaypparit@gmail.com>
2025-11-23 18:40:17 +0530

    todolist updated

--- 04 - States/states-with-array-and-objects/src/components/Todos.jsx
+++ 04 - States/states-with-array-and-objects/src/components/Todos.jsx
@@ -2,21 +2,37 @@ import Todo from "./Todo";
 
 const Todos = (props) => {
   const { todo, setTodo } = props;
-    
+
+  const handleCheck = (index) => {
+    const updated = [...todo];
+    updated[index].completed = !updated[index].completed;
+    setTodo(updated);
+  };
   return (
     <div>
       <Todo todo={todo} setTodo={setTodo} />
-      {
-        todo.map((item,index)=>{
-            return (
-              <div key={index} className="list">
-                <input type="checkbox" />
-                <span className="todo-txt">{item.task}</span>
-                <button className="del-btn">delete</button>
-              </div>
-            );
-        })
-      }
+      {todo.map((item, index) => {
+        return (
+          <div key={index} className="list">
+            <input
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => {
+                handleCheck(index);
+              }}
+            />
+            <span
+              style={{
+                textDecoration: item.completed ? "line-through" : "none",
+              }}
+              className="todo-txt"
+            >
+              {item.task}
+            </span>
+            <button className="del-btn">delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 };
